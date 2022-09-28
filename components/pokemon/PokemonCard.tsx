cbe627c858e33fd2753ca5e9ecb1bf3ae1266fa2
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-09-27 20:33:03 -0500

    Update Cards Pokemons and Create PokemonPage

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,16 +1,27 @@
 import { FC } from "react";
 import { Card, Grid, Row, Text } from "@nextui-org/react";
 import { SmallPokemon } from "../../interfaces";
+import { useRouter } from "next/router";
 
 interface Props {
   pokemon: SmallPokemon;
 }
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
   const { id, name, img } = pokemon;
+  const router = useRouter();
+
+  const onclick = () => {
+    router.push(`/pokemon/${id}`);
+  };
 
   return (
     <Grid key={id} xs={6} sm={4} md={2} xl={2}>
-      <Card isHoverable isPressable variant="bordered" css={{ mw: "400px" }}>
+      <Card
+        isHoverable
+        isPressable
+        variant="bordered"
+        css={{ mw: "400px" }}
+        onClick={onclick}>
         <Card.Body css={{ p: 1 }}>
           <Card.Image src={img} width="100%" height={140} alt={name} />
         </Card.Body>
