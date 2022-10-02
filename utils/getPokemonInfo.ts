64676a2d64630632e30354cea917c80ff9daf54e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-10-02 12:26:39 -0500

    Update Configuration for ISG by Id and Name

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,20 +3,24 @@ import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
 
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-  const sprites = {
-    back_default: data.sprites.back_default,
-    back_shiny: data.sprites.back_shiny,
-    front_default: data.sprites.front_default,
-    front_shiny: data.sprites.front_shiny,
-    other: data.sprites.other,
-  };
+    const sprites = {
+      back_default: data.sprites.back_default,
+      back_shiny: data.sprites.back_shiny,
+      front_default: data.sprites.front_default,
+      front_shiny: data.sprites.front_shiny,
+      other: data.sprites.other,
+    };
 
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: sprites,
-  };
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: sprites,
+    };
+  } catch (error) {
+    return null;
+  }
 
 }
