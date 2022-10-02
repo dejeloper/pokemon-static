04120f099bab799134a47a50cf5178f22522017f
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-10-01 23:17:46 -0500

    Add service of same api with name or id

---
+++ utils/getPokemonInfo.ts
@@ -0,0 +1,22 @@
+import { pokeApi } from "../api";
+import { Pokemon } from "../interfaces";
+
+export const getPokemonInfo = async (nameOrId: string) => {
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+
+  const sprites = {
+    back_default: data.sprites.back_default,
+    back_shiny: data.sprites.back_shiny,
+    front_default: data.sprites.front_default,
+    front_shiny: data.sprites.front_shiny,
+    other: data.sprites.other,
+  };
+
+  return {
+    id: data.id,
+    name: data.name,
+    sprites: sprites,
+  };
+
+}
